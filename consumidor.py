bc2f2c0cf4ea50ed7b4b0327982d967d118a8a68
rogerio-oraculo <rogerio.oraculo@gmail.com>
2024-10-14 02:14:29 +0200

    🟢

--- consumidor.py
+++ consumidor.py
@@ -7,25 +7,24 @@ from playwright.async_api import Playwright, async_playwright
 async def run(playwright: Playwright) -> None:
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
 
-    #browser = await playwright.chromium.launch(headless=False)
-    browser = await playwright.chromium.launch()
+    browser = await playwright.chromium.launch(headless=True)
     context = await browser.new_context(user_agent=user_agent)
     page = await context.new_page()
 
     # Vai para a página de download
     await page.goto("https://consumidor.gov.br/pages/dadosabertos/externo/")
-    await page.get_by_label("Mostrar", exact=False).select_option("50")
-
+    await page.locator("[name='publicacoesDT_length']").select_option("50", timeout=60000)
+    
     # Define o caminho para salvar o arquivo .zip
-    download_path = "C:/Users/Rogerio/Documents/playwright/downloads/finalizadas_2024-07.zip"  # Caminho onde o arquivo zip será salvo
-    extract_path = "C:/Users/Rogerio/Documents/playwright/dados/"  # Pasta para onde os arquivos serão extraídos
+    download_path = "C:/Users/Rogerio/Documents/consumidor_glassdoor_playwright/downloads/finalizadas_2024-01.zip"  # Caminho onde o arquivo zip será salvo
+    extract_path = "C:/Users/Rogerio/Documents/consumidor_glassdoor_playwright/dados/"  # Pasta para onde os arquivos serão extraídos
 
     # Garante que o diretório de extração exista
     os.makedirs(extract_path, exist_ok=True)
     
-    async with page.expect_download(timeout=1300000) as download_info:
+    async with page.expect_download(timeout=60000) as download_info:
         async with page.expect_popup() as page1_info:
-            await page.get_by_title("Download 'finalizadas_2024-07").click(timeout=1300000)
+            await page.get_by_title("Download 'finalizadas_2024-01").click(timeout=60000)
         page1 = await page1_info.value
 
     # Captura o download e salva o arquivo .zip no local especificado
